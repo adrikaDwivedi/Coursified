d69aed11034035ef1fe9a836cb8ba2778ab0a0f5
AdrikaD <adrika@Adrikas-MacBook-Air.local>
2026-05-06 21:39:13 +0530

    animation

---
+++ component/animation.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const animation = () => {
+  return (
+    <View>
+      <Text>animation</Text>
+    </View>
+  )
+}
+
+export default animation
+
+const styles = StyleSheet.create({})
